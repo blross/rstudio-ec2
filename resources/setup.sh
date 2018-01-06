a96070fc3fad992f2cb1e36bcced750932291824
Brendan Ross <br3ross@gmail.com>
2018-01-06 15:39:42 -0500

    Add setup script

---
+++ resources/setup.sh
@@ -0,0 +1,4 @@
+sudo chmod a+x launch_rs.py save_rs.py shutdown_rs.py
+ln -s launch_rs.py /usr/local/bin/launch_rs
+ln -s save_rs.py /usr/local/bin/save_rs
+ln -s shutdown_rs.py /usr/local/bin/shutdown_rs
